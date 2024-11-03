73a9f4e7a451e9cb5fe92382f6de77009d7461e2
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-03 22:15:36 +1100

    Basic setup for a platformer and secret inputs

---
+++ prototyping/keyboard_hint.gd
@@ -0,0 +1,31 @@
+extends Sprite2D
+
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var player: PlatformerController2D = $"../../Player"
+@onready var area_2d: Area2D = $Area2D
+@onready var timer: Timer = $Timer
+
+var player_near: bool = false
+
+func _process(_delta: float) -> void:
+	if !player_near: return
+	
+	if timer.is_stopped():
+		if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
+			timer.start()
+	else:
+		if not Input.is_action_pressed("left") or not Input.is_action_pressed("right"):
+			timer.stop()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == player:
+		player_near = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body == player:
+		player_near = false
+
+
+func _on_timer_timeout() -> void:
+	print("You did it!")
---
+++ prototyping/path_puzzle.gd
@@ -0,0 +1,31 @@
+extends Sprite2D
+@onready var player: PlatformerController2D = $"../../Player"
+
+var player_near: bool = false
+var code: Array[String] = ["up", "up", "right", "up", "up", "left", "left", "down"]
+var index: int = 0
+
+func _input(event: InputEvent) -> void:
+	if !player_near or index >= code.size(): return
+	var inputs: Array[String] = ["up", "right", "left", "down"]
+	for input in inputs:
+		if event.is_action_pressed(input):
+			if code[index] == input: index += 1
+			else: index = 0
+			print(index)
+			
+			if index >= code.size():
+				print("You found the secret path!")
+	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == player: player_near = true
+	index = 0
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body == player: player_near = false
+
+
+func _on_timer_timeout() -> void:
+	print("You did it!")
