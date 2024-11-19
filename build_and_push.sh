091dcf8465fa96eae5f6725383be682f47258182
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-19 19:01:25 +1100

    Can export, compile, and push to itch with 1 command now

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,3 +4,5 @@ godot4_3 --export-debug --headless Web
 echo "Export complete"
 zip -r builds/Web.zip builds/Web
 echo "Zip complete"
+./butler/butler push builds/Web.zip ThisIsRob/github-game-off-2024:web --userversion 1.0.0
+echo "Pushed to itch"
