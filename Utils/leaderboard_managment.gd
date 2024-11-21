b51a2f262ba049f8df9d129233802250b84f84b8
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-21 15:24:35 +1100

    Save the leaderboard before deleting

--- Utils/leaderboard_managment.gd
+++ Utils/leaderboard_managment.gd
@@ -1,12 +1,17 @@
 extends Control
 
 
-func _on_download_pressed() -> void:
+func save_leaderboard() -> void:
 	var sw_result: Dictionary = await SilentWolf.Scores.get_scores(0).sw_get_scores_complete
 	var time_stamp: String = Time.get_datetime_string_from_datetime_dict(Time.get_datetime_dict_from_system(), false)
 	var file = FileAccess.open("res://Speedrunning/LeaderboardBackups/leaderboard"+time_stamp+".json", FileAccess.WRITE)
 	file.store_string(str(sw_result))
 
 
+func _on_download_pressed() -> void:
+	save_leaderboard()
+
+
 func _on_clear_pressed() -> void:
+	save_leaderboard()
 	SilentWolf.Scores.wipe_leaderboard()
