17e44bf1d7d8228d23c5534b5f66d9a25d86ec45
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-21 15:22:17 +1100

    Added a scene to save and clear the leaderboard

---
+++ Utils/leaderboard_managment.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_download_pressed() -> void:
+	var sw_result: Dictionary = await SilentWolf.Scores.get_scores(0).sw_get_scores_complete
+	var time_stamp: String = Time.get_datetime_string_from_datetime_dict(Time.get_datetime_dict_from_system(), false)
+	var file = FileAccess.open("res://Speedrunning/LeaderboardBackups/leaderboard"+time_stamp+".json", FileAccess.WRITE)
+	file.store_string(str(sw_result))
+
+
+func _on_clear_pressed() -> void:
+	SilentWolf.Scores.wipe_leaderboard()
