db65231846328f1a30e43ba8c2b9f3bcb8640f80
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-25 18:32:37 +1100

    Added linux and windows builds to the build system

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,8 +1,10 @@
-rm -rf builds/Web
-mkdir builds/Web
-godot4_3 --export-debug --headless Web
+rm -rf builds
+mkdir -p builds/Web builds/Linux builds/Windows
+godot4_3 --export-release --headless Web
+godot4_3 --export-release --headless Linux
+godot4_3 --export-release --headless "Windows Desktop"
 echo "Export complete"
-zip -r builds/Web.zip builds/Web
-echo "Zip complete"
-./butler/butler push builds/Web.zip ThisIsRob/notepad-ace:web --userversion 1.0.0
+./butler/butler push builds/Web ThisIsRob/notepad-ace:web --userversion 1.0.0
+./butler/butler push builds/Linux/ ThisIsRob/notepad-ace:linux --userversion 1.0.0
+./butler/butler push builds/Windows ThisIsRob/notepad-ace:win-64 --userversion 1.0.0
 echo "Pushed to itch"
