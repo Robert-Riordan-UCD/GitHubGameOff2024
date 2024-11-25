b81f047e5a95573a1c7e1ef155447af045f73c54
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-25 13:42:47 +1100

    Updated leaderboard management to work with all leaderboards

--- Utils/leaderboard_managment.gd
+++ Utils/leaderboard_managment.gd
@@ -1,17 +1,21 @@
 extends Control
 
+var leaderboards: Array[String] = ["main", "0", "1", "2", "3"]
 
-func save_leaderboard() -> void:
-	var sw_result: Dictionary = await SilentWolf.Scores.get_scores(0).sw_get_scores_complete
-	var time_stamp: String = Time.get_datetime_string_from_datetime_dict(Time.get_datetime_dict_from_system(), false)
-	var file = FileAccess.open("res://Speedrunning/LeaderboardBackups/leaderboard"+time_stamp+".json", FileAccess.WRITE)
-	file.store_string(str(sw_result))
+
+func save_leaderboards() -> void:
+	for leaderboard in leaderboards:
+		var sw_result: Dictionary = await SilentWolf.Scores.get_scores(0, leaderboard).sw_get_scores_complete
+		var time_stamp: String = Time.get_datetime_string_from_datetime_dict(Time.get_datetime_dict_from_system(), false)
+		var file = FileAccess.open("res://Speedrunning/LeaderboardBackups/"+leaderboard+"/leaderboard"+time_stamp+".json", FileAccess.WRITE)
+		file.store_string(str(sw_result))
 
 
 func _on_download_pressed() -> void:
-	save_leaderboard()
+	save_leaderboards()
 
 
 func _on_clear_pressed() -> void:
-	save_leaderboard()
-	SilentWolf.Scores.wipe_leaderboard()
+	save_leaderboards()
+	for leaderboard in leaderboards:
+		SilentWolf.Scores.wipe_leaderboard(leaderboard)
