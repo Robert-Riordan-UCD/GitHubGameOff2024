c7a0f10a00fb904713b1bf15162bf73e408975a7
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-26 19:29:03 +1100

    Fixed bug with jump hint flickering

--- GUI/movement_hint.gd
+++ GUI/movement_hint.gd
@@ -15,7 +15,7 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	if Input.get_axis("left", "right"):
+	if need_move_hint and Input.get_axis("left", "right"):
 		need_move_hint = false
 		dismiss_move_hint()
 
