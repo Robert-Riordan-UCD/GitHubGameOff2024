069777922fa857eefcf4a177bd49484bb044d199
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-27 13:26:16 +1100

    Fixed bug where coming 10th wouldn't put you in the top 10

--- Speedrunning/leaderboard.gd
+++ Speedrunning/leaderboard.gd
@@ -93,11 +93,15 @@ func fetech_top_ten() -> void:
 func update_top_ten(score, player_name) -> bool:
 	var player_position: int = 0
 	for player in top_ten:
-		if score < player.score: break
+		if score < player.score:
+			top_ten.insert(player_position, Score.new(player_name, score, true))
+			return true
 		player_position += 1
-	if player_position >= 9: return false
-	top_ten.insert(player_position, Score.new(player_name, score, true))
-	return true
+	if player_position <= 10:
+		top_ten.insert(player_position, Score.new(player_name, score, true))
+		return true
+	return false
+
 
 
 # TODO: Handle error result
