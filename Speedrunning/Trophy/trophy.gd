7607d9a68b0906159a0f687101c23193b0ca0242
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-28 15:34:50 +1100

    Added particles to the trophies when collected

--- Speedrunning/Trophy/trophy.gd
+++ Speedrunning/Trophy/trophy.gd
@@ -4,6 +4,7 @@ signal collected
 
 @onready var area_2d: Area2D = $Area2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var cpu_particles_2d: CPUParticles2D = $CPUParticles2D
 
 func _ready() -> void:
 	animation_player.play("idle")
@@ -13,6 +14,7 @@ func _on_area_2d_body_entered(_body: Node2D) -> void:
 	collected.emit()
 	area_2d.set_deferred("monitoring", false)
 	animation_player.play("collected")
+	cpu_particles_2d.emitting = true
 
 
 func reset() -> void:
