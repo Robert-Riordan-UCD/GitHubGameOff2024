3a031a1e5fd29ccf0a11ad871eeb4bb8ff02ad00
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-28 13:31:06 +1100

    Added the ablity to pause

--- Speedrunning/speedrun_timer.gd
+++ Speedrunning/speedrun_timer.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var start_time: float = Time.get_unix_time_from_system()
 @onready var label: Label = $Label
+@onready var accumulated_time: float = 0
+
 var stopped: bool = false
 var stop_time: float
 
@@ -16,11 +18,27 @@ func stop_timer():
 	stopped = true
 
 
+func resume_timer():
+	stopped = false
+	accumulated_time = stop_time
+	start_time = Time.get_unix_time_from_system()
+
+
 func reset_timer():
 	start_time = Time.get_unix_time_from_system()
 	stopped = false
+	accumulated_time = 0
 
 
 func get_run_time():
 	if stopped: return stop_time
-	return snapped(Time.get_unix_time_from_system() - start_time, 0.001)
+	return snapped(Time.get_unix_time_from_system() - start_time + accumulated_time, 0.001)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().paused = not get_tree().paused
+		if get_tree().paused:
+			stop_timer()
+		else:
+			resume_timer()
