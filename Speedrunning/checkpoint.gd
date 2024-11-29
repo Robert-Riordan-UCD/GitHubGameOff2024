5871701ac50c8115fa033e35127a2383f2ad89f5
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-11-29 14:54:50 +1100

    Added particles to the checkpoints

--- Speedrunning/checkpoint.gd
+++ Speedrunning/checkpoint.gd
@@ -4,6 +4,7 @@ extends Node2D
 signal reached(checkpoint: Checkpoint)
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var cpu_particles_2d: CPUParticles2D = $CPUParticles2D
 @onready var activated: bool = false
 
 
@@ -20,6 +21,7 @@ func appear() -> void:
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(sprite_2d, "rotation", 0, 0.4).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
 	tween.play()
+	cpu_particles_2d.emitting = true
 
 
 func reset() -> void:
